ef6b18f9471191b0544cf94db022228bf6393d38
Cappossy <cappossy@gmail.com>
2025-06-19 08:51:40 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://cappossy.github.io/Fateless-SMP/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://82.65.23.170:8080/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
